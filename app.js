39ce8f6b81bbcd234ba0291f00c1b15d9e779222
40106038 <mmcgowan19@qub.ac.uk>
2024-04-01 15:02:42 +0100

    pages link with one another

--- app.js
+++ app.js
@@ -1,25 +1,55 @@
-const express = require("express");
-const app = express();
-app.set("view engine", "ejs");
-app.use(express.static(__dirname + "/public"));
-const axios = require('axios');
+const express = require('express');
+let app = express();
 
-app.use(express.urlencoded({ extended: true }));
+//middleware to use the EJS template engine
+app.set('view engine', 'ejs');
 
+//middleware to be able POST <form> data 
+app.use(express.urlencoded({extended: true}));
 
+//store favourite band in a global variable
+let favband = "not choosen yet";
 
-app.get("/", (req, res) => {
-    axios.get("http://jbusch.webhosting2.eeecs.qub.ac.uk/tvapi/?shows").then(results => {
-        let showsdata = results.data;
-        res.render('index', {showsdata});
-        })
-        .catch(err => {
-            console.log("Error: ", err.message);
-    });
+// DISPLAY HOME PAGE
+app.get('/',  (req, res) => {
+    res.render('index');
 });
 
+// DISPLAY HOME PAGE
+app.get('/views/index.ejs',  (req, res) => {
+    res.render('index');
+});
 
+app.post('/fav',  (req, res) => {
+    //change global variable favband to the value of the text field
+    favband = req.body.favourite;
+    res.render('index', {data: favband});
+});
 
-app.listen(3000, () => {
-    console.log("Server is running at port 3000");
+app.get('/views/about.ejs',  (req, res) => {
+    res.render('about');
+});
+
+app.get('/views/howtoplay.ejs',  (req, res) => {
+    res.render('howtoplay');
+});
+
+app.get('/views/howtoplay.ejs',  (req, res) => {
+    res.render('howtoplay', {data: favband});
+});
+
+app.get('/login',  (req, res) => {
+    res.render('login');
+});
+
+app.get('/signup',  (req, res) => {
+    res.render('signup');
+});
+
+app.get('/createblog',  (req, res) => {
+    res.render('createblog');
+});
+
+app.listen(process.env.PORT || 3001, ()=>{ 
+    console.log("server started on: localhost:3000/fav");
 });
